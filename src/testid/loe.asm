5665c2d81530fb7fe9846113c549bd909f7862d9
Märt Põder <boamaod@gmail.com>
2026-01-13 23:55:04 +0200

    Palju toredaid loendureid

--- src/testid/loe.asm
+++ src/testid/loe.asm
@@ -2,29 +2,62 @@
 boot	equ	0000h	;Bdos entry point
 ;
 ;	RomBios functions
-TTStat	equ	0ffc7h	;Keyboard status
-TTCon	equ	0ffcdh	;Send message to console
-OUThex	equ	0ffdfh	;Out hex word
-TMCnt	equ	0ffa1h	;Timer reset/read
+ttstat	equ	0ffc7h	;Keyboard status
+ttcon	equ	0ffcdh	;Send message to console
+outhexb	equ	0ffdch	;Out hex byte
+outhex	equ	0ffdfh	;Out hex word
+tmcnt	equ	0ffa1h	;Timer reset/read
 ;
 ;	Other
 cr	equ	0dh	;carriage return
 lf	equ	0ah	;line feed
 esc	equ	1bh	;escape code
+tim	equ	17h	;8253 timer
 ;
 	lxi	b,pais	;Start message
 	call	ttcon	;To console
 ;
 reset:	mvi	e,0
-	call	TMCnt	;Reset timer
+	call	tmcnt	;Reset timer
 ;
 mloop:	call	ttstat	;Check for pressed keys
 	jnz	action	;choose action
 	mvi	e,0ffh
-	call	TMCnt
+	call	tmcnt
 	mov	b,d
 	mov	c,e
 	call	outhex	;hexis n3itamine
+	lxi	b,space
+	call	ttcon
+	;mvi	a,00000000b
+	;out	tim
+	;lhld	0d463h
+	in	tim-3
+	call	outhexb
+	mov	b,a
+	in	tim-3
+	mov	c,a
+	;call	outhex
+	lxi	b,space
+	call	ttcon
+	mvi	a,01000000b
+	out	tim
+	;lhld	0d463h
+	in	tim-2
+	mov	b,a
+	in	tim-2
+	mov	c,a
+	;call	outhex
+	lxi	b,space
+	call	ttcon
+	mvi	a,10000000b
+	out	tim
+	;lhld	0d463h
+	in	tim-1
+	mov	b,a
+	in	tim-1
+	mov	c,a
+	;call	outhex
 	lxi	b,home	;move cursor home
 	call	ttcon
 	jmp	mloop
@@ -40,11 +73,12 @@ kyll:	lxi	b,clear	;clear screen
 ;
 ;	Data area
 ;
-pais:	db	esc,''''
+pais:	db	esc,27h ; ''' 
 	db	'P~sivara TIMCNT loenduri test 2025',cr,lf
 	db	esc,'(',cr,lf
 	db	'Katkestamiseks ESC, nullimiseks muu!'
 	db	cr,lf,0
+space:	db	' ',0
 clear:	db	cr,lf,0
 home:	db	cr,lf,esc,'A',0
 	end
